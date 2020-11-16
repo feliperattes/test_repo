05a8d941b629a275d1101314a2baa486137e66fc
feliperattes <feliperiffrattes@gmail.com>
2020-11-16 17:44:36 -0300

    Initial commit

---
+++ package_test/lib.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2018 Jean Bizot <jean@styckr.io>
+""" Main lib for package_test Project
+"""
+
+from os.path import split
+import pandas as pd
+import datetime
+
+def try_me():
+    print('Hi from Git and Felipe!')
